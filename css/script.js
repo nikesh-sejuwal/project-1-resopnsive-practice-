d8c41b47bf6ee2389b3f7afeba6ae6a6d92edf19
nikesh-sejuwal <125644709+nikesh-sejuwal@users.noreply.github.com>
2023-08-01 14:38:02 +0545

    Added Hamburger

--- css/script.js
+++ css/script.js
@@ -1,24 +1,34 @@
-document.getScroll = function () {
-   if (window.pageYOffset != undefined) {
-      return [pageXOffset, pageYOffset];
-   } else {
-      var sx,
-         sy,
-         d = document,
-         r = d.documentElement,
-         b = d.body;
-      sx = r.scrollLeft || b.scrollLeft || 0;
-      sy = r.scrollTop || b.scrollTop || 0;
-      return [sx, sy];
-   }
-};
-document.addEventListener("scroll", () => {
-   pos = document.getScroll();
-   min = 0;
-   max = 1117;
-   normal = pos[1] / (max - min);
-   end = `${normal * 1 * 100}%`;
-   mid = `${normal * 0.35 * 100}%`;
-   document.querySelector(":root").style.setProperty("--end", end);
-   document.querySelector(":root").style.setProperty("--mid", mid);
+document.addEventListener("DOMContentLoaded", function () {
+   const menu_btn = document.querySelector(".hamburger");
+   const mobile_menu = document.querySelector(".mobile_nav");
+
+   menu_btn.addEventListener("click", function () {
+      menu_btn.classList.toggle("is_active");
+      mobile_menu.classList.toggle("is_active");
+   });
+
+   document.getScroll = function () {
+      if (window.pageYOffset != undefined) {
+         return [pageXOffset, pageYOffset];
+      } else {
+         var sx,
+            sy,
+            d = document,
+            r = d.documentElement,
+            b = d.body;
+         sx = r.scrollLeft || b.scrollLeft || 0;
+         sy = r.scrollTop || b.scrollTop || 0;
+         return [sx, sy];
+      }
+   };
+   document.addEventListener("scroll", () => {
+      pos = document.getScroll();
+      min = 0;
+      max = 1117;
+      normal = pos[1] / (max - min);
+      end = `${normal * 1 * 100}%`;
+      mid = `${normal * 0.35 * 100}%`;
+      document.querySelector(":root").style.setProperty("--end", end);
+      document.querySelector(":root").style.setProperty("--mid", mid);
+   });
 });
